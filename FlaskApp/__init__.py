20f91861b669f1ccf38352eeab15ec60e411c4f7
Rudy van Sloten <rdvansloten@outlook.com>
2023-09-13 16:34:04 +0200

    Fix Pipeline trigger

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -13,26 +13,34 @@ AZURE_DEVOPS_PROJECT_NAME = os.environ["AZURE_DEVOPS_PROJECT_NAME"]
 AZURE_DEVOPS_ORGANIZATION_NAME = os.environ["AZURE_DEVOPS_ORGANIZATION_NAME"]
 
 def trigger_pipeline(personal_access_token, organization_name, project_name, pipeline_id):
-    url = f"https://dev.azure.com/{organization_name}/{project_name}/_apis/build/builds?api-version=6.0"
-    
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Basic {base64.b64encode(personal_access_token)}',
-    }
+    personal_access_token = personal_access_token.encode('ascii')
+    personal_access_token = base64.b64encode(personal_access_token)
+    personal_access_token = personal_access_token.decode('ascii')
 
-    data = {
-        "definition": {
-            "id": pipeline_id
+    url = f"https://dev.azure.com/{organization_name}/{project_name}/_apis/pipelines/{pipeline_id}/runs?api-version=7.0&Content-Type=application/json"
+
+    payload = json.dumps({
+      "resources": {
+        "repositories": {
+          "self": {
+            "refName": "refs/heads/main"
+          }
         }
+      }
+    })
+    headers = {
+      'Content-Type': 'application/json',
+      'Authorization': f'Basic {personal_access_token}',
     }
 
-    response = requests.post(url, headers=headers, json=data)
-    
+    response = requests.request("POST", url, headers=headers, data=payload)
+
     if response.status_code == 200:
         print("Pipeline triggered successfully.")
     else:
         print(f"Failed to trigger the pipeline. Status code: {response.status_code}")
-        print(response.json())
+        print(response.json)
+
 
 # Initialize the connection to Azure storage account
 blob_service_client = BlobServiceClient.from_connection_string(STORAGE_ACCOUNT_CONNECTION_STRING)
