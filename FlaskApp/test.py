31ac681207f9b81d3f1a917f88758be008a6f229
Rudy van Sloten <rdvansloten@outlook.com>
2023-09-13 16:28:07 +0200

    add tf dir

--- FlaskApp/test.py
+++ FlaskApp/test.py
@@ -1,26 +1,35 @@
 import requests
 import base64
+import json
 
 def trigger_pipeline(personal_access_token, organization_name, project_name, pipeline_id):
     
     personal_access_token = personal_access_token.encode('ascii')
     personal_access_token = base64.b64encode(personal_access_token)
     personal_access_token = personal_access_token.decode('ascii')
-    print(personal_access_token)
 
-    url = f"https://dev.azure.com/{organization_name}/{project_name}/_apis/pipelines/{pipeline_id}/runs?api-version=7.0"
+    url = f"https://dev.azure.com/{organization_name}/{project_name}/_apis/pipelines/{pipeline_id}/runs?api-version=7.0&Content-Type=application/json"
 
+    payload = json.dumps({
+      "resources": {
+        "repositories": {
+          "self": {
+            "refName": "refs/heads/main"
+          }
+        }
+      }
+    })
     headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Basic :{personal_access_token}',
+      'Content-Type': 'application/json',
+      'Authorization': f'Basic {personal_access_token}',
     }
 
-    response = requests.post(url, headers=headers)
-    
+    response = requests.request("POST", url, headers=headers, data=payload)
+
     if response.status_code == 200:
         print("Pipeline triggered successfully.")
     else:
         print(f"Failed to trigger the pipeline. Status code: {response.status_code}")
-        print(response)
+        print(response.json)
 
-trigger_pipeline("72tpkmsmr7b3korkx53xbogosyecivnonzd6kfxybr2buunmbbea", "rdvansloten", "Automation", "28")
+trigger_pipeline(":72tpkmsmr7b3korkx53xbogosyecivnonzd6kfxybr2buunmbbea", "rdvansloten", "Automation", "28")
